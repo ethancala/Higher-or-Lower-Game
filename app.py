7ff6f20c2a292ea61dc3e0aa2c635d4c63a77930
Ethan <ecala420@gmail.com>
2024-04-10 23:59:28 -0500

    This is the functional version of the higher or lower game. Currently the game does not use any card mechanics and only bets of 1-100 numbers. This will change. Old version repository was made private. Updated UI and improved database structure

--- app.py
+++ app.py
@@ -10,16 +10,20 @@ client = MongoClient('mongodb://localhost:27017/')
 db = client['higher_lower_game']
 collection = db['game_sessions']
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/play')
 def play():
     session.clear()  # Clear any existing game session
     session['round'] = 1
     session['player_wins'] = 0
+    session['rounds_lost'] = 0  # Initialize rounds lost counter
     return redirect('/guess')
+
 
 @app.route('/guess', methods=['GET', 'POST'])
 def guess():
@@ -32,48 +36,67 @@ def guess():
 
     if request.method == 'POST':
         guess = request.form['guess']
-        session['guesses'] += 1
+
+        # Reset guesses to 0 for each new round
+        session['guesses'] = 0
 
         if (guess == 'higher' and session['next_number'] > session['number']) or \
-           (guess == 'lower' and session['next_number'] < session['number']):
+                (guess == 'lower' and session['next_number'] < session['number']):
             session['player_wins'] += 1
             session['round'] += 1
             if session['player_wins'] >= 3 or session['round'] > 5:
                 game_status = "Won" if session['player_wins'] >= 3 else "Lost"
-                save_game_result(session['player_wins'], session['next_number'], session['guesses'])
+                save_game_result(session['player_wins'], session['rounds_lost'], game_status)
                 session.clear()
                 return redirect('/result')
             else:
                 session['number'] = session['next_number']
                 session['next_number'] = random.randint(1, 100)
-                session['guesses'] = 0
-                return render_template('guess.html', message='You won this round! On to the next round.', round=session['round'])
+                return render_template('guess.html', message='You won this round! On to the next round.',
+                                       round=session['round'])
 
         else:
-            message = 'Incorrect! Try again.'
+            session['rounds_lost'] += 1  # Increment rounds lost counter
+            if session['rounds_lost'] >= 3:
+                game_status = "Lost"
+                save_game_result(session['player_wins'], session['rounds_lost'], game_status)
+                session.clear()
+                return redirect('/result')
+            else:
+                session['round'] += 1
+                session['number'] = session['next_number']
+                session['next_number'] = random.randint(1, 100)
+                return render_template('guess.html', message='Incorrect!', round=session['round'])
+
+
     else:
         session['next_number'] = random.randint(1, 100)
         message = 'Is the next number higher or lower?'
 
     return render_template('guess.html', message=message, round=session['round'])
 
-def save_game_result(player_wins, number_to_guess, guesses):
-    game_result = {'player_wins': player_wins, 'number_to_guess': number_to_guess, 'guesses': guesses}
+
+def save_game_result(player_wins, rounds_lost, outcome):
+    game_result = {'player_wins': player_wins, 'rounds_lost': rounds_lost, 'outcome': outcome}
     collection.insert_one(game_result)
+
 
 @app.route('/result')
 def result():
     game_history = collection.find()
     return render_template('result.html', game_history=game_history)
 
+
 @app.route('/games_history')
 def games_history():
     game_history = collection.find()
     return render_template('games_history.html', game_history=game_history)
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
